b5d5ca48edf0b14697c63b60e53bb883302bff5d
Krystallia Savvaki <csd3380@csd.uoc.gr>
2019-06-15 18:26:57 +0300

    Evaluator dispatcher

--- include/utilities/Evaluator.h
+++ include/utilities/Evaluator.h
@@ -1,20 +1,16 @@
 #pragma once
 
 #include <string>
+#include <map>
 #include "utilities/Object.h"
 
 class Evaluator
 {
 public:
-	Evaluator();
-	//~Evaluator();
-	//Value* EvaluateVar(ASTnode* node);
-	//Value* EvaluateAddExpr(ASTnode* node);
-	//Value* EvaluateIfStmt(ASTnode* node);
-
-	////generic evaluate 
-	//Value* Evaluate(ASTnode* node);
+	Evaluator() = default;
+	Value Evaluate(ASTnode* node);
 private:
-
+	static std::map<std::string, Value (Evaluator::*)(ASTnode*) > EvaluateDispatcher;
+	Value EvaluateAddExpr(ASTnode* node);
 };
 
